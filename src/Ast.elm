5ef7dbdaa8f902cc80e55e51df72bf62384ee1cf
Pap Lőrinc <paplorinc@gmail.com>
2017-01-21 17:18:04 +0200

    Reformatted the example application's JSON source

--- src/Ast.elm
+++ src/Ast.elm
@@ -30,52 +30,52 @@ function = map3 Function (field "name"    <| string)
 
 ast : Dict String Function
 ast = decodeString (list function) """
-   [{
-     "name": "solve the quadratic equation (𝕔x² + 𝕓x + 𝕒 = 0)",
-     "inputs": ["𝕔","𝕓","𝕒"],
-     "outputs": {"-𝕓±√Δ\\n————\\n 2𝕔":
-        {"divide": [{"plus and minus": [{"negate": ["𝕓"]}
-                                      ,{"sqrt": {"subtract":[{"square": ["𝕓"]}
-                                                            ,{"multiply": ["4","𝕔","𝕒"]}]}}]}
-                  ,{"multiply": ["2","𝕔"]}]}}
-   }, {
-     "name": "solve the linear equation (𝕓x + 𝕒 = 0)",
-     "inputs": ["𝕓","𝕒"],
-     "outputs": {"-𝕒\\n——\\n𝕓": {"negate": {"divide": ["𝕒","𝕓"]}}}
-   }, {
-     "name": "negate",
-     "inputs": ["𝕒"],
-     "outputs": {"-𝕒": {"scala.math.Numeric.negate": ["𝕒"]}}
-   }, {
-     "name": "add",
-     "inputs": ["𝕒","𝕓"],
-     "outputs": {"𝕒+𝕓": {"scala.math.Numeric.plus": ["𝕒","𝕓"]}}
-   }, {
-     "name": "subtract",
-     "inputs": ["𝕒","𝕓"],
-     "outputs": {"𝕒-𝕓": {"scala.math.Numeric.minus": ["𝕒","𝕓"]}}
-   }, {
-     "name": "multiply",
-     "inputs": ["𝕒","𝕓"],
-     "outputs": {"𝕒×𝕓": {"scala.math.Numeric.times": ["𝕒","𝕓"]}}
-   }, {
-     "name": "divide",
-     "inputs": ["𝕒","𝕓"],
-     "outputs": {"𝕒\\n——\\n𝕓": {"scala.math.Numeric.FloatIsFractional.div": ["𝕒","𝕓"]}}
-   }, {
-     "name": "square",
-     "inputs": ["𝕒"],
-     "outputs": {"𝕒²": {"multiply": ["𝕒","𝕒"]}}
-   }, {
-     "name": "square root",
-     "inputs": ["𝕒"],
-     "outputs": {"√𝕒": {"java.lang.Math.sqrt": ["𝕒"]}}
-   }, {
-     "name": "plus and minus",
-     "inputs": ["𝕒","𝕓"],
-     "outputs": {"𝕒+𝕓": {"add":      ["𝕒","𝕓"]}
-                ,"𝕒-𝕓": {"subtract": ["𝕒","𝕓"]}}
-   }]
-""" |> Result.withDefault []
-    |> List.map (\x -> (x.name, x))
-    |> Dict.fromList
+[{
+    "name":    "solve the quadratic equation (𝕔x² + 𝕓x + 𝕒 = 0)",
+    "inputs":  ["𝕔", "𝕓", "𝕒"],
+    "outputs": { "-𝕓±√Δ\\n————\\n 2𝕔":
+                   {"divide": [{"plus and minus": [{"negate": ["𝕓"]}
+                                                  ,{"sqrt": {"subtract":[{"square": ["𝕓"]}
+                                                                        ,{"multiply": ["4","𝕔","𝕒"]}]}}]}
+                              ,{"multiply": ["2","𝕔"]}]}}
+}, {
+  "name":    "solve the linear equation (𝕓x + 𝕒 = 0)",
+  "inputs":  ["𝕓", "𝕒"],
+  "outputs": {"-𝕒\\n——\\n𝕓": {"negate": {"divide": ["𝕒", "𝕓"]}}}
+}, {
+  "name":    "negate",
+  "inputs":  ["𝕒"],
+  "outputs": {"-𝕒": {"scala.math.Numeric.negate": ["𝕒"]}}
+}, {
+  "name":    "add",
+  "inputs":  ["𝕒", "𝕓"],
+  "outputs": {"𝕒+𝕓": {"scala.math.Numeric.plus": ["𝕒", "𝕓"]}}
+}, {
+  "name":    "subtract",
+  "inputs":  ["𝕒", "𝕓"],
+  "outputs": {"𝕒-𝕓": {"scala.math.Numeric.minus": ["𝕒", "𝕓"]}}
+}, {
+  "name":    "multiply",
+  "inputs":  ["𝕒", "𝕓"],
+  "outputs": {"𝕒×𝕓": {"scala.math.Numeric.times": ["𝕒", "𝕓"]}}
+}, {
+  "name":    "divide",
+  "inputs":  ["𝕒", "𝕓"],
+  "outputs": {"𝕒\\n——\\n𝕓": {"scala.math.Numeric.FloatIsFractional.div": ["𝕒", "𝕓"]}}
+}, {
+  "name":    "square",
+  "inputs":  ["𝕒"],
+  "outputs": {"𝕒²": {"multiply": ["𝕒", "𝕒"]}}
+}, {
+  "name":    "square root",
+  "inputs":  ["𝕒"],
+  "outputs": {"√𝕒": {"java.lang.Math.sqrt": ["𝕒"]}}
+}, {
+  "name":    "plus and minus",
+  "inputs":  ["𝕒", "𝕓"],
+  "outputs": { "𝕒+𝕓": {"add": ["𝕒", "𝕓"]},
+               "𝕒-𝕓": {"subtract": ["𝕒", "𝕓"]}
+  }
+}]""" |> Result.withDefault []
+      |> List.map (\x -> (x.name, x))
+      |> Dict.fromList
