71f2cf9c1f63f176bb85a01fd09da37e2332054f
Pap Lőrinc <paplorinc@gmail.com>
2017-01-26 21:51:39 +0200

    Text alignment changes

--- src/Lists.elm
+++ src/Lists.elm
@@ -1,3 +1,3 @@
 module Lists exposing (..)
 
-maxLength strings = Maybe.withDefault 0 (List.maximum (List.map String.length strings))
+maxLength strings = Maybe.withDefault 0 (List.maximum (List.map String.length strings)) |> toFloat
