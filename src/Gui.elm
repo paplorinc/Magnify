82b16f4476dcbd07b54aeffaaebf52aa02c58a7a
Pap Lőrinc <paplorinc@gmail.com>
2017-01-26 14:52:37 +0200

    Added output parameters rendering

--- src/Gui.elm
+++ src/Gui.elm
@@ -9,7 +9,8 @@ import Maybe exposing ( Maybe(..) )
 import Svg exposing (Svg)
 import Svg.Attributes exposing (..)
 import Task
-import Tuple4 exposing (mapAll)
+import Tuple2
+import Tuple4
 import VirtualDom
 import Window
 
@@ -30,21 +31,28 @@ arrowHead = Svg.marker [ id "arrowHead"
                        , orient "auto"
                        , fill "red"
                        ] [ Svg.path [ d "M 0 0 L 10 5 L 0 10 z" ] [] ]
-                       
+
+mapFirstRest : List a -> (a -> b) -> (a -> b) -> List b
+mapFirstRest list mapFirst mapRest = case list of
+                                          first::rest -> mapFirst first :: List.map mapRest rest
+                                          [] -> []
 
 parameter : Int -> Int -> Int -> String -> Svg Msg
 parameter x_ y_ width name = let
-                                 (lineX1, lineX2, lineY1, lineY2) = (x_ |> toString, x_ + width |> toString, y_ |> toString, y_ |> toString)
-                                 (textX, textY) = (x_ + (width // 2) |> toString, lineY1)
-                                 centerX = String.length name // 2 |> toString
+                                 (lineX1, lineX2, lineY1, lineY2) = (x_, x_ + width, y_, y_) |> Tuple4.mapAll toString
+                                 (textX, textY) = (x_ + (width // 2), y_) |> Tuple2.mapBoth toString
+                                 format text = let lines = String.split "\n" text
+                                                   first line = Svg.text line
+                                                   next line  = Svg.tspan [ x textX, dy "0.5em" ] [ Svg.text line ]
+                                               in mapFirstRest lines first next
                              in Svg.g [] [ Svg.line [ x1 lineX1, x2 lineX2, y1 lineY1, y2 lineY1, stroke "red", strokeWidth "2" , markerEnd "url(#arrowHead)" ] []
-                                         , Svg.text_ [ x textX, y textY, dx <| String.concat [ "-", centerX, "em" ] ] [ Svg.text name ] ]
+                                         , Svg.text_ [ x textX, y textY, textAnchor "middle" ] (format name) ]
 
 
 function : String -> Svg Msg
 function name = let
                     (x_, y_, w_, h_) = (100, 100, 100, 100)
-                    (posX, posY, sizeH, sizeV) = mapAll toString (x_, y_, w_, h_)
+                    (posX, posY, sizeH, sizeV) = (x_, y_, w_, h_) |> Tuple4.mapAll toString
 
                     radius = sqrt 100 |> toString
 
@@ -52,12 +60,17 @@ function name = let
                                         |> Maybe.map (\f -> (f.inputs, f.outputs))
                                         |> Maybe.withDefault ([], Dict.empty)
 
-                    params = let
-                                 width = 50
+                    params = let width = 60
                                  spacing = h_ // (1 + List.length inputs)
                                  x = x_ - width
-                                 y = \i -> y_ + (i + 1) * spacing
+                                 y index = y_ + (index + 1) * spacing
                              in inputs |> List.indexedMap (\i name -> parameter x (y i) width name)
 
-                    returns = Dict.values outputs
-                in Svg.g [] ([ Svg.rect [ x posX, y posY, width sizeH, height sizeV, rx radius, ry radius, fill "gray" ] [] ] ++ params)
+                    returns = let outputValues = Dict.keys outputs
+                                  width = 60
+                                  x = x_ + w_ - (width // 2)
+                                  spacing = h_ // (1 + List.length outputValues)
+                                  y = \i -> y_ + (i + 1) * spacing
+                              in outputValues |> List.indexedMap (\i name -> parameter x (y i) width name)
+
+                in Svg.g [] ([ Svg.rect [ x posX, y posY, width sizeH, height sizeV, rx radius, ry radius, fill "gray" ] [] ] ++ params ++ returns)
